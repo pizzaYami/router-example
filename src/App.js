2fd661ef1c15e1297ef0cf54007c39b6138dd6c6
pizzaYami <cyj9407@naver.com>
2024-03-26 23:13:08 +0900

    Feat: productDetailPage

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import AboutPage from "./page/AboutPage.tsx";
 import HomePage from "./page/HomePage.tsx";
+import ProductPage from "./page/ProductPage.tsx";
+import ProductDetailPage from "./page/ProductDetailPage.tsx";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
+        <Route path="/product" element={<ProductPage />} />
+        <Route path="/product/:id" element={<ProductDetailPage />} />
       </Routes>
     </div>
   );
--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -1,11 +1,16 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function HomePage() {
+  const navigate = useNavigate();
+  const goProductPage = () => {
+    navigate("/product?q=pants");
+  };
   return (
     <div>
       <h1>HomePage</h1>
       <Link to="/about">Go to about Page</Link>
+      <button onClick={goProductPage}></button>
     </div>
   );
 }
